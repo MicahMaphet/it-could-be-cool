b269ef92a3327ccc08e989fbe911ab7594cf4f1b
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-07-02 02:39:58 +0000

    added ai file

---
+++ ai1.js
@@ -0,0 +1,12 @@
+var x = 0;
+var y = 0;
+var gx = 0;
+var gy = 0;
+
+function goto() {
+  gx = document.getElementById("ai2")
+}
+function movement() {
+  ai1.style.top = y + "px";
+  ai1.style.left = x + "px";
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+for(i = 0; i < 1000; i++) {
+  goto();
+}
+
+function test() {
+  alert("yo yo yo it worked");
+}
